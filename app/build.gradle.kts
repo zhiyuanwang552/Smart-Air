f281c6ebe4b981b808dcc8a95204bd8c98cb953f
dylaine <dylanling707@gmail.com>
2025-12-01 19:23:35 -0500

    feat: added chart generation to R6 dashboard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,9 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.database)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
--- app/src/main/java/com/example/b07demosummer2024/ParentHomeActivity.java
+++ app/src/main/java/com/example/b07demosummer2024/ParentHomeActivity.java
@@ -1,7 +1,14 @@
 package com.example.b07demosummer2024;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,17 +16,26 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -28,8 +44,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
@@ -39,12 +59,20 @@ public class ParentHomeActivity extends Fragment {
     private Spinner childSpinner;
     private Button setPbButton;
     private Button exportButton;
+    private Switch chartToggle;
     private AlertAdapter alertAdapter;
     private RecyclerView recyclerView;
     private List<String> childNameList;
     private FirebaseDatabase db;
     private DatabaseReference childRef;
     private List<AlertInstance> alertList;
+    private LineChart lineChart;
+    private DatabaseReference zoneRef;
+    private ArrayList<Entry> entries;
+    private CardView cardOne;
+    private CardView cardTwo;
+    private CardView cardThree;
+    private TextView infoText;
 
     private final FirebaseAuth myAuth = FirebaseAuth.getInstance();
 
@@ -58,6 +86,11 @@ public class ParentHomeActivity extends Fragment {
         setPbButton = view.findViewById(R.id.setPbButton);
         zoneDisplay = view.findViewById(R.id.zoneDisplay);
         childSpinner = view.findViewById(R.id.childSpinner);
+        cardOne = view.findViewById(R.id.cardOne);
+        cardTwo = view.findViewById(R.id.cardTwo);
+        cardThree = view.findViewById(R.id.cardThree);
+        chartToggle = view.findViewById(R.id.chartToggle);
+        infoText = view.findViewById(R.id.infoText);
         childNameList = new ArrayList<>();
         recyclerView = view.findViewById(R.id.recycler_alerts);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
@@ -78,6 +111,13 @@ public class ParentHomeActivity extends Fragment {
         recyclerView.setAdapter(alertAdapter);
 
         reloadPage("blank");
+
+        exportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exportReport(requireContext());
+            }
+        });
 
         childSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -153,11 +193,27 @@ public class ParentHomeActivity extends Fragment {
     }
     private void reloadPage(String childId){
         if (childId.equals("blank")){
-            // hide things, set defaults.
+            hideDisplays();
             return;
         }
+        showDisplays();
         setZoneDisplay(childId);
         fetchAlertsFromDatabase(childId);
+        if (chartToggle.isChecked()) {
+            generateLineChart(30, childId);
+        } else {
+            generateLineChart(7, childId);
+        }
+        chartToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    generateLineChart(30, childId);
+                } else {
+                    generateLineChart(7, childId);
+                }
+            }
+        });
         setPbButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -168,6 +224,23 @@ public class ParentHomeActivity extends Fragment {
                 loadFragment(PEF);
             }
         });
+        // add onclick listener for export button.
+    }
+
+    private void hideDisplays(){
+        cardOne.setVisibility(View.INVISIBLE);
+        cardTwo.setVisibility(View.INVISIBLE);
+        cardThree.setVisibility(View.INVISIBLE);
+        recyclerView.setVisibility(View.INVISIBLE);
+        infoText.setText("Select a child to view the dashboard!");
+    }
+
+    private void showDisplays(){
+        cardOne.setVisibility(View.VISIBLE);
+        cardTwo.setVisibility(View.VISIBLE);
+        cardThree.setVisibility(View.VISIBLE);
+        recyclerView.setVisibility(View.VISIBLE);
+        infoText.setText("Recent Alerts");
     }
 
     private void fetchAlertsFromDatabase(String childId) {
@@ -240,6 +313,104 @@ public class ParentHomeActivity extends Fragment {
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 // Handle possible errors
             }
+        });
+    }
+
+    private void exportReport(Context context) {
+        PdfDocument report = new PdfDocument();
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(595, 842, 1).create();
+        PdfDocument.Page page = report.startPage(pageInfo);
+
+        Canvas canvas = page.getCanvas();
+        Paint paint = new Paint();
+        paint.setTextSize(16);
+        canvas.drawText("SAMPLE TEXTTTTTTTTT", 75, 100, paint);
+        canvas.drawText("PDFPDFPDF", 75, 125, paint);
+
+        lineChart.setDrawingCacheEnabled(true);
+        lineChart.buildDrawingCache();
+        Bitmap chartBitmap = Bitmap.createBitmap(lineChart.getWidth(), lineChart.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas chartCanvas = new Canvas(chartBitmap);
+        lineChart.draw(chartCanvas);
+        Rect src = new Rect(0, 0, chartBitmap.getWidth(), chartBitmap.getHeight()); // original bitmap
+        Rect dst = new Rect(75, 150, 75 + 300, 150 + 150);
+        canvas.drawBitmap(chartBitmap, src, dst, null);
+
+        report.finishPage(page);
+
+        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+                + "/example.pdf";
+
+        File file = new File(path);
+
+        try (OutputStream out = new FileOutputStream(file)) {
+            report.writeTo(out);
+            Toast.makeText(context, "Saved to Downloads (legacy)", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            report.close();
+        }
+
+    }
+
+    private void generateLineChart(int numOfDays, String childId) {
+        // List of last 7/30 days
+        List<String> dates = new ArrayList<>();
+        for (int i = 0; i < numOfDays; i++) {
+            String date = java.time.LocalDate.now().minusDays(i).toString();
+            dates.add(date);
+        }
+
+        ArrayList<String> daysOfMonth = new ArrayList<>();
+        for (int j = 0; j < dates.size(); j++) {
+            daysOfMonth.add(dates.get(j).split("-")[2]);
+        }
+
+        zoneRef = db.getReference("children/" + childId + "/pefHistory");
+        zoneRef.get().addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+                Toast.makeText(getContext(), "Failed to generate chart", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (!entries.isEmpty()) {
+                entries.clear();
+            }
+
+            DataSnapshot snapshot = task.getResult();
+            for (int i = 0; i < dates.size(); i++) {
+                String date = dates.get(i);
+                DataSnapshot daySnapshot = snapshot.child("/" + date + "/zonePercentage");
+                Integer value = daySnapshot.getValue(Integer.class);
+                if (value != null) {
+                    entries.add(new Entry(numOfDays - 1 - i, value));
+                } else {
+                    entries.add(new Entry(numOfDays - 1 - i, 0));
+                }
+            }
+
+            Collections.reverse(entries);
+            Collections.reverse(daysOfMonth);
+
+            // Sets up line information
+            LineDataSet dataSet = new LineDataSet(entries, "Sample Data");
+            dataSet.setColor(Color.parseColor("#0000FF"));
+            dataSet.setValueTextColor(Color.parseColor("#000000"));
+            dataSet.setLineWidth(2f);
+
+            // Additional configuration for the chart
+            lineChart.getDescription().setEnabled(false);
+            lineChart.getAxisRight().setEnabled(false);
+            lineChart.getLegend().setEnabled(false);
+            lineChart.getAxisLeft().setAxisMinimum(0);
+            lineChart.getAxisLeft().setAxisMaximum(100);
+            lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+            lineChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(daysOfMonth));
+
+            // Generates chart
+            lineChart.setData(new LineData(dataSet));
+            lineChart.invalidate();
         });
     }
 
